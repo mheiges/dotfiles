384b457c52ec9f3210baa3adfbb95fa916915d3f
Mark Heiges <mheiges@gmail.com>
2018-09-04 09:07:07 -0400

    change bash completion setup for Docker

--- .bash-completions/docker.sh
+++ .bash-completions/docker.sh
@@ -1,4 +1,7 @@
 # Docker bash completion
+if [[ -f $(brew --prefix)/etc/bash_completion ]]; then
+  source $(brew --prefix)/etc/bash_completion
+fi
 if [ -d /Applications/Docker.app/Contents/Resources ]; then
  . /Applications/Docker.app/Contents/Resources/etc/docker.bash-completion
  . /Applications/Docker.app/Contents/Resources/etc/docker-machine.bash-completion
