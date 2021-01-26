06cd417ee9c192b7ad47ca8dda193d0e241accc4
Mark Heiges <mark.heiges@semanticbits.com>
2021-01-25 19:06:23 -0500

    fix path to docker bash completion script

--- .bash-completions/docker.sh
+++ .bash-completions/docker.sh
@@ -4,7 +4,6 @@ if [[ -f $(brew --prefix)/etc/bash_completion ]]; then
 fi
 if [ -d /Applications/Docker.app/Contents/Resources ]; then
  . /Applications/Docker.app/Contents/Resources/etc/docker.bash-completion
- . /Applications/Docker.app/Contents/Resources/etc/docker-machine.bash-completion
  . /Applications/Docker.app/Contents/Resources/etc/docker-compose.bash-completion
 fi
 
