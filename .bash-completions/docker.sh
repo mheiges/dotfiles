a78a370e916e53ceb9f6944e1d54acedf6dd7582
Mark Heiges <mark.heiges@semanticbits.com>
2021-06-25 10:38:52 -0400

    docker help message

--- .bash-completions/docker.sh
+++ .bash-completions/docker.sh
@@ -1,6 +1,9 @@
 # Docker bash completion
+# Requires `brew install bash-completion`
 if [[ -f $(brew --prefix)/etc/bash_completion ]]; then
   source $(brew --prefix)/etc/bash_completion
+else
+  echo 'Run `brew install bash-completion` for Docker bash completion'
 fi
 if [ -d /Applications/Docker.app/Contents/Resources ]; then
  . /Applications/Docker.app/Contents/Resources/etc/docker.bash-completion
